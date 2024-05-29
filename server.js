83eb160538fbb715310e5336eb24226059b845c3
Ranem <ranemart19@.com>
2024-05-29 16:38:06 +0300

    Update server.js to connect to MariaDB

--- server.js
+++ server.js
@@ -1,40 +1,24 @@
-const mysql = require('mysql');
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.json());
+
 const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST || 'mysql',
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    host: process.env.MARIADB_HOST || '127.0.0.1',
+    user: process.env.MARIADB_USER || 'root',
+    password: process.env.MARIADB_PASSWORD || '',
+    database: process.env.MARIADB_DATABASE || 'restaurantdb',
 });
 
 db.connect((err) => {
     if (err) {
-        console.error('Error connecting to MySQL:', err);
+        console.error('Error connecting to MariaDB:', err);
         return;
     }
-    console.log('Connected to MySQL');
-    const createTableSql = `
-        CREATE TABLE IF NOT EXISTS restaurants (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            restaurant_name VARCHAR(255) NOT NULL,
-            description TEXT,
-            location VARCHAR(255),
-            sponsor_name VARCHAR(255)
-        )
-    `;
-    db.query(createTableSql, (err, result) => {
-        if (err) {
-            console.error('Error creating table:', err);
-            return;
-        }
-        console.log('Table created or already exists.');
-    });
+    console.log('Connected to MariaDB');
 });
-
-app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to the QKSA API!');
